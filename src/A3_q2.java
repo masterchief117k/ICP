a0160d5969e8e06269a283b0405eac1c2e4b22ff
masterchief117k <76084163+masterchief117k@users.noreply.github.com>
2022-12-22 08:56:17 +0530

    important updates

--- src/A3_q2.java
+++ src/A3_q2.java
@@ -1,3 +1,21 @@
+/*Alice visited SUM hospital to get treatment for her fever and illness. Doctor advised 
+her to drink at least 5000 ml of water each day. Alice drank x ml of water today. Write 
+a program that print the following message depending on the value of x 
+“Yes, Alice is following doctor’s advice” 
+OR
+“No, Alice is not following doctor’s advice”*/
+import java.util.*;
 public class A3_q2 {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the amount of water you have drunk today(in ml) ");
+        int water;
+         water = sc.nextInt();
+        if(water<5000){
+            System.out.println("No, Alice is not following doctors advice");
+        }else{
+            System.out.println("Yes, Alice is following doctors advice");
+        }
+
+    }
 }
