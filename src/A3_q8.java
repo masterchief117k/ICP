fe684ba696b2ad829f9202ccef105cb3c5bd582c
masterchief117k <76084163+masterchief117k@users.noreply.github.com>
2023-01-06 19:45:04 +0530

    update 3

---
+++ src/A3_q8.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class A3_q8 {
+   public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter x coordinates");
+    double x=sc.nextDouble();
+    System.out.println("enter y coordinates");
+    double y = sc.nextDouble(); 
+    if (x > 0 && y > 0){
+    System.out.println("lies in First quadrant");
+    }
+    else if (x < 0 && y > 0){
+     System.out.println("lies in Second quadrant");
+    } 
+    else if (x < 0 && y < 0){
+     System.out.println("lies in Third quadrant");
+    }
+    else if (x > 0 && y < 0){
+     System.out.println("lies in Fourth quadrant");
+    }
+    else if (x == 0 && y > 0){
+     System.out.println("lies at positive y axis");
+    }
+    else if (x == 0 && y < 0){
+      System.out.println("lies at negative y axis");
+    }
+    else if (y == 0 && x < 0){
+     System.out.println("lies at negative x axis");
+    }
+    else if (y == 0 && x > 0){
+     System.out.println("lies at positive x axis");
+    }
+    else{
+     System.out.println("lies at origin");
+    }
+}
+
+}
+
