4e3ea546f97c5fcfee3668a8448659d27200c35d
masterchief117k <76084163+masterchief117k@users.noreply.github.com>
2023-01-11 08:54:39 +0530

    major update

--- src/Assingment_4/A4_q3.java
+++ src/Assingment_4/A4_q3.java
@@ -1,5 +1,35 @@
+/*Question-3: 
+Write a java program that takes an integer N from user, uses 
+Math.  Random  ()  to  print  N  random  integer  numbers  between  1  to  N,  and  then  prints  their 
+average value. Use do while loop. 
+Sample run: 
+Enter a number: 6 
+Random numbers generated are: 6 4 5 2 2 5 
+Average of 6 random numbers are 4 */
 package Assingment_4;
-
+import java.lang.*;
+import java.util.*;
 public class A4_q3 {
+    public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    int n,r,count=0;
+    System.out.println("Enter the number ");
+    n=sc.nextInt();
+    int sum=0,div=0;
+   System.out.print("The randomly generated number are ");
+    do{
+       r= 1+((int)((Math.random())*((n-1)+1)));
+       count=count+r;
+       System.out.print( r+" ");
+       sum++;
     
+    }
+    while(sum<=n-1);
+   int avrage=count/n;
+    System.out.println("\nAvrage of "+n+" numbers are "+avrage);
+        
+
+    
+     
+  }
 }
---
+++ src/Assingment_4/A4_q4.java
@@ -0,0 +1,27 @@
+/*Write  a  program  that  finds  greatest  common  divisor  (GCD)  of  two  numbers  using  Euclid's 
+algorithm, which is an iterative computation based on the following observation: if y divides 
+x, the GCD of x and y is y; otherwise, the GCD of x and y is same as GCD of x % y and y. 
+ 
+Sample run: 
+Enter the first number: 56 
+Enter the second number: 98 
+GCD of 56 and 98 is 14. */
+package Assingment_4;
+import java.util.*;
+public class A4_q4 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter the first number: ");
+    int x = input.nextInt();
+    System.out.print("Enter the second number: ");
+    int y = input.nextInt();
+
+    while (y != 0) {
+        int temp = y;
+        y = x % y;
+        x = temp;
+    }
+
+    System.out.println("GCD: " + x);
+}
+}
